f1d074ffb476057af5ae9341fcb673bad03e2ced
Alexey Petrov <alexey.petrov@kaspi.kz>
2017-08-14 14:44:51 +0600

    initialize

---
+++ src/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="list">
+    {{ todolist }}
+    <list-item v-for="(item, key) in items" :item="item" :key="key"></list-item>
+  </div>
+</template>
+
+<script>
+import listItem from './ListItem';
+
+export default {
+  components: { listItem },
+  name: 'list',
+  data() {
+    return {
+      todolist: 'To-do list',
+      items: [
+        { title: 'Vue' },
+        { titile: 'Unit Testing' },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+  .list {
+    background: greenyellow;
+  }
+</style>
+
+
+
---
+++ src/components/ListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ item }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'listItem',
+  data() {
+    return {
+      item: 'Default value for item',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
+
+
